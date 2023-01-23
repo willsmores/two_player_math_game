1cb96c308699c4a349f77bba1e08fed980cfa2d1
Steve Willsmore <scwillsmore@gmail.com>
2023-01-23 18:50:18 -0500

    Add game and question logic.

--- classes/game.rb
+++ classes/game.rb
@@ -1,3 +1,72 @@
 class Game 
 
+  def initialize
+    @current_player = nil
+  end
+
+  # Create 2 new players
+  def create_players
+    puts "Player 1 Name: "
+    player1 = $stdin.gets.chomp
+    @player1 = Player.new(player1)
+    puts "Player 2 Name: "
+    player2 = $stdin.gets.chomp
+    @player2 = Player.new(player2)
+
+    puts "It's the #{@player1.name} and #{@player2.name} maths quiz!"
+  end
+
+  def set_current_player
+    if @current_player == nil
+      @current_player = @player1
+    elsif @current_player == @player1
+      @current_player = @player2
+    else @current_player == @player2
+      @current_player = @player1
+    end
+    @current_player
+  end
+  
+  def play_game
+    create_players
+
+    # Runs each round of questions... 
+    loop do
+      # New round begins
+      self.set_current_player
+
+      new_question = Question.new(@current_player)
+
+      print "#{@current_player.name}: "
+      new_question.ask_question
+      answer = $stdin.gets.chomp
+
+      if new_question.evaluate_answer(answer)
+        puts "Yes that's correct!"
+      else
+        puts "Whoops, no that's not right."
+        @current_player.lose_life
+      end
+
+      # ...until someone loses all lives
+      if @current_player.lives == 0
+        announce_winner
+        break
+      else
+        puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
+      end
+
+    end
+  end
+
+  def announce_winner
+    if @current_player == @player1
+      puts "#{@player2.name} wins with a score of #{@player2.lives}/3!"
+      puts "Good bye!"
+    else 
+      puts "#{@player1.name} wins with a score of #{@player1.lives}/3!"
+      puts "Good bye!"
+    end
+  end
+
 end
--- classes/question.rb
+++ classes/question.rb
@@ -1,3 +1,37 @@
 class Question
 
+  attr_accessor :total
+
+  def initialize(current_player)
+    random_numbers
+    total
+  end
+
+  def random_numbers
+    @value1 = rand(20)
+    @value2 = rand(20)
+  end
+
+  def total
+    @total = @value1 + @value2
+  end
+
+  def ask_question
+    puts "What does #{@value1} plus #{@value2} equal?"
+    # @answer = $stdin.gets.chomp
+    # evaluate_answer
+  end
+
+  def evaluate_answer(answer)
+    answer.to_i == @total
+  end
+
+  # def evaluate_answer
+  #   if @answer.to_i != @total
+  #     puts "Whoops, no that's not right."
+  #     @current_player.lose_life
+  #   else
+  #     puts "Yes that's correct!"
+  #   end
+  # end
 end
